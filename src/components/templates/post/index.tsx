c554f748f6def3ee5f5857927b720daacab066fd
Chris McKenna <hello@mckennach.com>
2024-10-21 15:41:26 -0400

    removed styles

--- src/components/templates/post/index.tsx
+++ src/components/templates/post/index.tsx
@@ -3,7 +3,6 @@ import { print } from 'graphql/language/printer'
 import { ContentNode, Post } from '@/gql/graphql'
 import { fetchGraphQL } from '@/src/lib/api/fetchGraphQL'
 
-import styles from './PostTemplate.module.css'
 import { POST_QUERY } from '@/lib/queries'
 
 interface TemplateProps {
@@ -16,9 +15,9 @@ export default async function PostTemplate({ node }: TemplateProps) {
   })
 
   return (
-    <div className={styles.post}>
-      <h1 className={styles.title}>{post.title}</h1>
-      <div className={styles.author}>By {post.author?.node.name}</div>
+    <div>
+      <h1>{post.title}</h1>
+      <div>By {post.author?.node.name}</div>
 
       <div dangerouslySetInnerHTML={{ __html: post.content || '' }} />
     </div>
