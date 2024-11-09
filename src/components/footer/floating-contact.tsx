e11b9cb9a3becb66f3333c2fe482c89036a51d10
Chris McKenna <hello@mckennach.com>
2024-11-09 11:22:07 -0500

    hiding on mobile

--- src/components/footer/floating-contact.tsx
+++ src/components/footer/floating-contact.tsx
@@ -5,7 +5,7 @@ import { Text } from "../ui/text";
 
 export default function FloatingContact() {
 	return (
-		<div className="fixed bottom-10 lg:right-24 z-40">
+		<div className="hidden lg:block fixed bottom-10 lg:right-24 z-40">
 			<Link href="/contact">
 				<Text type="label" className="text-cream inline-flex items-center gap-2">
 					CONTACT US <CustomIcons name="link" />
