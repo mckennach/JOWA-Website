85d2838090c67ce096d640ba0564a31504883b22
Chris McKenna <hello@mckennach.com>
2024-12-23 11:26:33 -0500

    testing static

--- next.config.js
+++ next.config.js
@@ -50,6 +50,15 @@ const nextConfig = {
       },
     ]
   },
+	serverRuntimeConfig: {
+		// Will only be available on the server side
+		 mySecret: 'secret',
+		 secondSecret: process.env.HEADLESS_SECRET, // Pass through env  variables
+	},
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		 staticFolder: '/static',
+	},
 }
 
 module.exports = nextConfig
